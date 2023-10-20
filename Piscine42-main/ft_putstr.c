45ae97cdf02daca44f14fc200b5e659eccd12af8
root <root@MSI.localdomain>
2023-10-20 03:56:12 +0200

    réorganisation du git 42

---
+++ Piscine42-main/ft_putstr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lberthal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 15:40:04 by lberthal          #+#    #+#             */
+/*   Updated: 2023/08/28 17:07:05 by lberthal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putstr(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while  (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+int main(void)
+{
+	char	src[] = "yo mon pote";
+	ft_putstr(src);
+}
