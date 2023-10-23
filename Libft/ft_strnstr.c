79b9339a9b2ff88f55d7cfa2acbe8486155940ef
Lucas Berthalon <lberthal@k0r3p4.42mulhouse.fr>
2023-10-23 22:01:00 +0200

    avancer sur libft n6 paco ok jusqu'a strnstr en cours de patch

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: lberthal <lberthal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 18:08:02 by lberthal          #+#    #+#             */
-/*   Updated: 2023/10/22 23:31:09 by lberthal         ###   ########.fr       */
+/*   Updated: 2023/10/23 19:39:58 by lberthal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char *ft_strnstr(const char *big, const char *little, size_t len)
 	
 	i = 0;
 	u = 0;
-	if (!little || (!big && !little))  
+	if (!*little) 
 		return ((char*)big);
 	if (!big)
 		return (NULL);
@@ -45,26 +45,24 @@ char *ft_strnstr(const char *big, const char *little, size_t len)
 		if (u != ft_strlen(little))
 			u = 0;
 		if (u == ft_strlen(little))
-			break;
+			return (str);
 		i++;
 		str++;
 	}
-	if (i < len)
-		return (str);
 	return (NULL);
 }
 
-int	main(void)
-{
-	// test of the original function
-	// printf("%s\n", strnstr("test dans le tester", "", 6));
-	char *result = strnstr("", "", 0);
-	if (result == NULL)
-		printf("c'est bien NULL\n");
+// int	main(void)
+// {
+// 	// test of the original function
+// 	// printf("%s\n", strnstr("test dans le tester", "", 6));
+// 	char *result = strnstr("", "", 0);
+// 	if (result == NULL)
+// 		printf("c'est bien NULL\n");
 
-	// test of my function
-	// printf("%s\n", ft_strnstr("test dans le tester", "", 6));
-	char *result1 = ft_strnstr("", "", 0);
-	if (result1 == NULL)
-		printf("c'est bien NULL\n");
-}
+// 	// test of my function
+// 	// printf("%s\n", ft_strnstr("test dans le tester", "", 6));
+// 	char *result1 = ft_strnstr("", "", 0);
+// 	if (result1 == NULL)
+// 		printf("c'est bien NULL\n");
+// }
