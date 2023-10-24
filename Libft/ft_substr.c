253eb662ddb2862f85e8eb551a7a4d1de6c17144
BerthalonLucas <berthalonlucas@gmail.com>
2023-10-24 12:04:49 +0200

    try substr a revoir

---
+++ Libft/ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lucas <lucas@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/24 11:57:03 by lucas             #+#    #+#             */
+/*   Updated: 2023/10/24 12:03:51 by lucas            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	str = malloc(sizeof(char) * (len + 1))
+	if (!str)
+		return (NULL);
+	while (i < len && s[start])
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int main(void)
+{
+	char *str = "Hello World";
+	
+	printf("%s\n", ft_substr(str, 2, 5));
+	return (0);
+}
