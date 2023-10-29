41e1aa97ac6177d15888f791eeb6102f9844f975
BerthalonLucas <berthalonlucas@gmail.com>
2023-10-29 18:43:35 +0100

    toujours sur split, premier push qui ressemble a quelque chose, reste main a faire pour tester

--- Libft/libft.h
+++ Libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lberthal <lberthal@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lucas <lucas@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 20:24:24 by lberthal          #+#    #+#             */
-/*   Updated: 2023/10/28 01:31:23 by lberthal         ###   ########.fr       */
+/*   Updated: 2023/10/28 22:25:48 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,10 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_tolower(int c);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char *ft_strjoin(char const *s1, char const *s2);
-char *ft_strtrim(char const *s1, char const *set);
-char *ft_itoa(int n);
-char **ft_split(char const *s, char c);
+char    *ft_strjoin(char const *s1, char const *s2);
+char    *ft_strtrim(char const *s1, char const *set);
+char    *ft_itoa(int n);
+char    **ft_split(char const *s, char c);
 
 
 
